f128d162eeaf55b389d9d321d6b74f880adbe36e
Angelo <angepili@icloud.com>
2020-05-31 21:00:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var convertExcel    = require('excel-as-json').processFile,
     argv            = require('yargs').argv,
-    fileName = argv.file,
-    getdate = new Date(),
-    date = getdate.getFullYear()+getdate.getMonth()+getdate.getDay()+'-'+getdate.getHours()+getdate.getMinutes()+getdate.getSeconds(),
-    fileNameDest =  fileName.split('.')[0]+'-'+date+'.'+fileName.split('.')[1].replace('xlsx','json');
+    fileName        = argv.file.split('.'),
+    fileNameDest    = '';
 
-    convertExcel('./exports/excels/'+fileName,'./exports/json/'+fileNameDest,false,function(err,data){
-        console.log('Json created with '+data.length+' products, in ./exports/json/'+fileNameDest);
+    fileNameDest =  filename[0]+'.'+filename[1].replace('xlsx','json');
+
+    convertExcel('./import/'+fileName,'./export/'+fileNameDest,false,function(err,data){
+        console.log('exported '+data.length+' rows);
     });
